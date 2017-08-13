da5fe2f7e0da28cd651258d63c71e3f0fca67ec7
Markus Fisch <mf@markusfisch.de>
2017-08-13 13:55:06 +0200

    Set SVG sources for action icons to target color

--- svg/update.sh
+++ svg/update.sh
@@ -104,18 +104,18 @@ svg/ic_launcher.svg 48
 EOF
 
 update app/src/main/res/drawable << EOF
-svg/ic_action_add.svg 24 1
+svg/ic_action_add.svg 24
 svg/ic_action_add_texture.svg 24
-svg/ic_action_cut.svg 24 1
-svg/ic_action_delete.svg 24 1
-svg/ic_action_duplicate.svg 24 1
-svg/ic_action_insert_code.svg 24 1
-svg/ic_action_insert_tab.svg 24 1
-svg/ic_action_preview.svg 24 1
-svg/ic_action_rotate_clockwise.svg 24 1
-svg/ic_action_run.svg 24 1
-svg/ic_action_save.svg 24 1
-svg/ic_action_settings.svg 24 1
-svg/ic_action_share.svg 24 1
-svg/ic_action_update.svg 24 1
+svg/ic_action_cut.svg 24
+svg/ic_action_delete.svg 24
+svg/ic_action_duplicate.svg 24
+svg/ic_action_insert_code.svg 24
+svg/ic_action_insert_tab.svg 24
+svg/ic_action_preview.svg 24
+svg/ic_action_rotate_clockwise.svg 24
+svg/ic_action_run.svg 24
+svg/ic_action_save.svg 24
+svg/ic_action_settings.svg 24
+svg/ic_action_share.svg 24
+svg/ic_action_update.svg 24
 EOF
