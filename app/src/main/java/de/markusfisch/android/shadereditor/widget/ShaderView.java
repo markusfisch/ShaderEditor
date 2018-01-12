db22e9e368484fa97ba8b1534c255ba5df941987
Markus Fisch <mf@markusfisch.de>
2018-01-12 21:56:35 +0100

    Remove unused GL10 import

--- app/src/main/java/de/markusfisch/android/shadereditor/widget/ShaderView.java
+++ app/src/main/java/de/markusfisch/android/shadereditor/widget/ShaderView.java
@@ -12,7 +12,6 @@ import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.egl.EGLContext;
 import javax.microedition.khronos.egl.EGLDisplay;
-import javax.microedition.khronos.opengles.GL10;
 
 public class ShaderView extends GLSurfaceView {
 	private ShaderRenderer renderer;
