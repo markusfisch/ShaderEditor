ef2ee411e43926ae09217143cac38034832b9656
Markus Fisch <mf@markusfisch.de>
2018-11-03 16:13:19 +0100

    Make sure streams get closed
    And specify a default encoding when importing a shader too.
    Just to make FindBugs happy.

--- app/src/main/java/de/markusfisch/android/shadereditor/database/ImportExport.java
+++ app/src/main/java/de/markusfisch/android/shadereditor/database/ImportExport.java
@@ -23,6 +23,9 @@ public class ImportExport {
 		try {
 			File importDirectory = getImportExportDirectory(context, false);
 			File[] files = importDirectory.listFiles();
+			if (files == null) {
+				return;
+			}
 
 			int successCount = 0;
 			int failCount = 0;
@@ -115,13 +118,15 @@ public class ImportExport {
 				Toast.makeText(context, str, Toast.LENGTH_LONG).show();
 			}
 		} catch (IOException e) {
-			Toast.makeText(context, context.getString(R.string.export_failed, e.getMessage()),
-					Toast.LENGTH_LONG).show();
+			Toast.makeText(context, context.getString(R.string.export_failed,
+					e.getMessage()), Toast.LENGTH_LONG).show();
 		}
 	}
 
-	private static void writeShaderToDirectory(File directory, String shaderName,
-											   String fragmentShader) throws IOException {
+	private static void writeShaderToDirectory(
+			File directory,
+			String shaderName,
+			String fragmentShader) throws IOException {
 		String filename = shaderName.replaceAll("[^a-zA-Z0-9 \\-_,()]",
 				ILLEGAL_CHARACTER_REPLACEMENT);
 		File shaderFile = new File(directory, filename + SHADER_FILE_EXTENSION);
@@ -131,38 +136,51 @@ public class ImportExport {
 					+ ")" + SHADER_FILE_EXTENSION);
 		}
 
-		FileOutputStream outputStream;
-		outputStream = new FileOutputStream(shaderFile);
-		outputStream.write(fragmentShader.getBytes("UTF-8"));
-		outputStream.close();
+		FileOutputStream os = null;
+		try {
+			os = new FileOutputStream(shaderFile);
+			os.write(fragmentShader.getBytes("UTF-8"));
+		} finally {
+			if (os != null) {
+				os.close();
+			}
+		}
 	}
 
-	private static File getImportExportDirectory(Context context, boolean create) throws IOException {
-		File downloadsDirectory =
-				Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+	private static File getImportExportDirectory(
+			Context context,
+			boolean create) throws IOException {
+		File downloadsDirectory = Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_DOWNLOADS);
 		File file = new File(downloadsDirectory, IMPORT_EXPORT_DIRECTORY);
 
-		if (create) {
-			//noinspection ResultOfMethodCallIgnored
-			file.mkdirs();
+		if (create && !file.mkdirs()) {
+			// throws in next block; just to make FindBugs happy
 		}
 
 		if (!file.isDirectory()) {
-			throw new IOException(context.getString(R.string.path_is_no_directory,
-					downloadsDirectory));
+			throw new IOException(context.getString(
+					R.string.path_is_no_directory, downloadsDirectory));
 		}
 
 		return file;
 	}
 
 	private static String readFile(File file) throws IOException {
-		FileInputStream fis = new FileInputStream(file);
-		StringBuilder content = new StringBuilder();
-		byte[] buffer = new byte[1024];
-		int n;
-		while ((n = fis.read(buffer)) != -1) {
-			content.append(new String(buffer, 0, n));
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file);
+			StringBuilder sb = new StringBuilder();
+			byte[] buffer = new byte[1024];
+			int n;
+			while ((n = fis.read(buffer)) != -1) {
+				sb.append(new String(buffer, 0, n, "UTF-8"));
+			}
+			return sb.toString();
+		} finally {
+			if (fis != null) {
+				fis.close();
+			}
 		}
-		return content.toString();
 	}
 }
