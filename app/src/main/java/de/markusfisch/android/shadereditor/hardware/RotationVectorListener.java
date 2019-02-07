a103f52b4c45f44fcf6726b422926b904f50df1d
Markus Fisch <mf@markusfisch.de>
2019-02-07 21:10:50 +0100

    Prefer TYPE_GAME_ROTATION_VECTOR if possible
    Because it doesn't depend from a geomagnetic sensor.

--- app/src/main/java/de/markusfisch/android/shadereditor/hardware/RotationVectorListener.java
+++ app/src/main/java/de/markusfisch/android/shadereditor/hardware/RotationVectorListener.java
@@ -3,6 +3,7 @@ package de.markusfisch.android.shadereditor.hardware;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.os.Build;
 
 public class RotationVectorListener extends AbstractListener {
 	public final float values[] = new float[]{0, 0, 0};
@@ -12,6 +13,12 @@ public class RotationVectorListener extends AbstractListener {
 	}
 
 	public boolean register() {
+		// prefer TYPE_GAME_ROTATION_VECTOR if possible because it doesn't
+		// depend from a geomagnetic sensor
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 &&
+				register(Sensor.TYPE_GAME_ROTATION_VECTOR)) {
+			return true;
+		}
 		return register(Sensor.TYPE_ROTATION_VECTOR);
 	}
 
