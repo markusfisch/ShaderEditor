5a404231b457340b2ce9b2fb18e33d1a2f064af0
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2023-06-15 22:37:26 +0200

    migrate to [AndroidX](https://developer.android.com/jetpack/androidx), because support.appcompat is [deprecated](https://developer.android.com/topic/libraries/support-library/packages)

--- app/src/main/java/de/markusfisch/android/shadereditor/activity/AddUniformActivity.java
+++ app/src/main/java/de/markusfisch/android/shadereditor/activity/AddUniformActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 
 import de.markusfisch.android.shadereditor.fragment.UniformPagesFragment;
 
--- app/src/main/java/de/markusfisch/android/shadereditor/widget/CubeMapView.java
+++ app/src/main/java/de/markusfisch/android/shadereditor/widget/CubeMapView.java
@@ -12,7 +12,7 @@ import android.graphics.RectF;
 import android.net.Uri;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.support.v4.content.ContextCompat;
+import androidx.core.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.ViewConfiguration;
