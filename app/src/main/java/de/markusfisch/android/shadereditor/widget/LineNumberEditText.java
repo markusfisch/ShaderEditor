db77041bedf789192e08589aaa8155f9dad75354
Markus Fisch <mf@markusfisch.de>
2023-07-20 13:16:31 +0200

    Make numDigits() static and add braces
    A method should be static if it can be so it's immediately clear
    that it doesn't use anything from its environment.
    
    Maintainability-wise, it's always smarter to use braces than not.
    
    People were bitten by skipping braces:
    https://www.imperialviolet.org/2014/02/22/applebug.html

--- app/src/main/java/de/markusfisch/android/shadereditor/widget/LineNumberEditText.java
+++ app/src/main/java/de/markusfisch/android/shadereditor/widget/LineNumberEditText.java
@@ -122,24 +122,29 @@ public class LineNumberEditText extends AppCompatEditText {
 	 * @param number the number of which you want to get the number of digits.
 	 * @return the number of decimal digits of the given {@code number}
 	 */
-	private int numDigits(int number) {
+	private static int numDigits(int number) {
 		if (number < 100000) {
 			if (number < 100) {
-				if (number < 10) return 1;
-				else return 2;
+				if (number < 10) {
+					return 1;
+				} else {
+					return 2;
+				}
 			} else if (number < 1000) {
 				return 3;
 			} else if (number < 10000) {
 				return 4;
-			} else return 5;
+			} else {
+				return 5;
+			}
 		} else if (number < 10000000) {
-			if (number < 1000000) return 6;
-			else return 7;
+			return number < 1000000 ? 6 : 7;
 		} else if (number < 100000000) {
 			return 8;
 		} else if (number < 1000000000) {
 			return 9;
-		} else return 10;
+		} else {
+			return 10;
+		}
 	}
-
 }
