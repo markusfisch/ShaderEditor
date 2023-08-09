815fdfe448b47587ae9fe65b32c61e9f88fe22cd
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2023-08-09 17:25:55 +0200

    Fix CharIterator, Add documentation
    Make CharIterator return
    Character.MAX_VALUE for positions
    that are out of bounds.

--- app/src/main/java/de/markusfisch/android/shadereditor/highlighter/CharIterator.java
+++ app/src/main/java/de/markusfisch/android/shadereditor/highlighter/CharIterator.java
@@ -6,31 +6,62 @@ import androidx.annotation.NonNull;
 public final class CharIterator {
 	private final @NonNull String source;
 	private int position = 0;
+
 	public CharIterator(@NonNull String source) {
 		this.source = source;
 	}
+
+	/**
+	 * Get the current character. If the current index out of bounds, {@code Character.MAX_VALUE}
+	 * is returned. This character is used, because it is invalid unicode.
+	 *
+	 * @return The current character.
+	 */
 	public char ch() {
-		return source.charAt(position);
+		return ch(position);
 	}
+
+	/**
+	 * Peek the next character. If the peeked index out of bounds, {@code Character.MAX_VALUE} is
+	 * returned. This character is used, because it is invalid unicode.
+	 *
+	 * @return The peeked character.
+	 */
 	public char peek() {
-		return peek(position + 1);
+		return ch(position + 1);
 	}
-	private char peek(int position) {
-		if (position >= source.length()) {
-			return 0;
+
+	/**
+	 * Get at a character at an absolute position {@code position}. If the current index out of
+	 * bounds, {@code Character.MAX_VALUE} is returned. This character is used, because it is
+	 * invalid unicode.
+	 *
+	 * @param position The absolute char position.
+	 * @return The char at the position.
+	 */
+	private char ch(int position) {
+		if (position < source.length()) {
+			return source.charAt(position);
 		}
-		return source.charAt(position);
+		return Character.MAX_VALUE;
 	}
+
+	/**
+	 * Increment the iterators position.
+	 */
 	public void next() {
 		++position;
 	}
 
 	/**
-	 * Peek next char, skipping C-style line continuations.
+	 * Peek next char, skipping C-style line continuations. If the peeked index out of bounds,
+	 * {@code Character.MAX_VALUE} is returned. This character is used, because it is invalid
+	 * unicode.
+	 *
 	 * @return the peeked char.
 	 */
 	public char peekC() {
-		char ch = 0;
+		char ch = Character.MAX_VALUE;
 		int position = this.position;
 		int length = source.length();
 		do {
@@ -41,6 +72,11 @@ public final class CharIterator {
 		return ch;
 	}
 
+	/**
+	 * Skip to the end of a newline, if there is any.
+	 *
+	 * @return whether there was a newline.
+	 */
 	private boolean moveNewline() {
 		final int start = position;
 		position = nextNewline(start);
@@ -49,7 +85,7 @@ public final class CharIterator {
 	}
 
 	/**
-	 * Go to next char, skipping C-style line continuations
+	 * Go to next char, skipping C-style line continuations.
 	 */
 	public void nextC() {
 		char ch;
@@ -59,12 +95,18 @@ public final class CharIterator {
 		} while (ch == '\\' && moveNewline());
 	}
 
+	/**
+	 * Get the position of the end of a newline at this position.
+	 *
+	 * @param iter the current position.
+	 * @return The position of the end of the newline. If there was none, `iter` is returned.
+	 */
 	private int nextNewline(int iter) {
-		char peek = peek(iter);
+		char peek = ch(iter);
 		// is next \r or \n? -> iterate
 		if (peek == '\r' || peek == '\n') {
 			++iter;
-			char second_peek = peek(iter);
+			char second_peek = ch(iter);
 			// is next \r\n or \n\r? -> iterate
 			if (second_peek != peek && (second_peek == '\r' || second_peek == '\n'))
 				++iter;
