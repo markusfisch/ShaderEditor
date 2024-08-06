a3eff936ce0abfd8a70c8fb1e8849ffaa11c0bb0
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2024-08-06 16:43:24 +0200

    Refactor build script to Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+	alias(libs.plugins.android.application)
+}
+
+android {
+	namespace = "de.markusfisch.android.shadereditor"
+
+	compileSdk = 34
+
+	defaultConfig {
+		minSdk = 21
+		targetSdk = compileSdk
+
+		versionCode = 85
+		versionName = "2.34.3"
+
+		vectorDrawables {
+			useSupportLibrary = true
+		}
+	}
+
+	signingConfigs {
+		create("release") {
+			keyAlias = System.getenv("ANDROID_KEY_ALIAS")
+			keyPassword = System.getenv("ANDROID_KEY_PASSWORD")
+			storePassword = System.getenv("ANDROID_STORE_PASSWORD")
+			storeFile = System.getenv("ANDROID_KEYFILE")?.let { file(it) }
+		}
+	}
+
+	buildTypes {
+		debug {
+			applicationIdSuffix = ".debug"
+		}
+
+		release {
+			isMinifyEnabled = true
+			isShrinkResources = true
+			signingConfig = signingConfigs["release"]
+		}
+	}
+
+	buildFeatures {
+		buildConfig = true
+		viewBinding = true
+	}
+
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+}
+
+dependencies {
+	implementation(libs.androidx.appcompat)
+	implementation(libs.material)
+	implementation(libs.androidx.preference)
+}
